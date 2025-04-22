b93caf4710878ccc2d9483c1fd1716ed1a37cbb0
TheCoolGeek <4321@example.com>
2025-04-22 22:52:16 +0300

    Added process biome after recieve

--- controller/minecraft_ac_controller/minecraft_ac_controller/minecraft_communicator.py
+++ controller/minecraft_ac_controller/minecraft_ac_controller/minecraft_communicator.py
@@ -1,28 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from process_biome import process_biome
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  # Get the size of data
         post_data = self.rfile.read(content_length)  # Read the data
         try:
-            # Parse the JSON data
             data = json.loads(post_data)
-            response = {
-                "status": "success",
-                "received": data
-            }
+            process_biome(data)
             self.send_response(200)
         except json.JSONDecodeError:
-            response = {
-                "status": "error",
-                "message": "Invalid JSON"
-            }
             self.send_response(400)
 
-        self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(response).encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
     server_address = ('', port)
---
+++ controller/minecraft_ac_controller/minecraft_ac_controller/process_biome.py
@@ -0,0 +1,4 @@
+from typing import Dict
+
+def process_biome(biome_data: Dict):
+    print(biome_data["biome"])
