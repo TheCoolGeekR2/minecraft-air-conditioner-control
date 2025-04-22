51df5747a9a23c541eeec7679bace1f9ef5bdc27
TheCoolGeek <4321@example.com>
2025-04-22 23:33:55 +0300

    print biome with parameters

--- controller/minecraft_ac_controller/process_biome.py
+++ controller/minecraft_ac_controller/process_biome.py
@@ -1,4 +1,8 @@
 from typing import Dict
+from .consts import BIOMES
 
 def process_biome(biome_data: Dict):
-    print(biome_data["biome"])
+    biome_id = biome_data["biome"]
+    ac_parameters = BIOMES[biome_id]
+    print(f"Biome ID: {biome_id}, AC Parameters: {ac_parameters}")
+    
