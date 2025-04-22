e3bc46b9570919b1fdcd4d627af235c180168332
TheCoolGeek <4321@example.com>
2025-04-22 21:53:32 +0300

    added python package structure

---
+++ controller/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='minecraft_ac_controller',
+    version='0.1',
+    author='TheCoolGeek', 
+    author_email='4321@example.com',
+    packages=find_packages(),
+    long_description=open('README.md').read()
+)
