111505e4a96741e8245ab77efc5e39290004759c
TheCoolGeek <TheCoolGeek@cool.com>
2025-04-25 01:24:12 +0300

    removed leading minecraft

--- controller/minecraft_ac_controller/process_biome.py
+++ controller/minecraft_ac_controller/process_biome.py
@@ -3,6 +3,7 @@ from .consts import BIOMES
 
 def process_biome(biome_data: Dict):
     biome_id = biome_data["biome"]
-    ac_parameters = BIOMES[biome_id]
+    biome_name = biome_id.replace("minecraft:", "")
+    ac_parameters = BIOMES[biome_name]
     print(f"Biome ID: {biome_id}, AC Parameters: {ac_parameters}")
     
