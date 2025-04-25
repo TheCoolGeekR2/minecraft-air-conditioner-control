3c94e0417c2daf35231ac13cb11905e9bbb069e6
TheCoolGeek <TheCoolGeek@cool.com>
2025-04-26 01:55:43 +0300

    moved main to ac_command script

--- controller/scripts/ac_command.py
+++ controller/scripts/ac_command.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer
-from .minecraft_communicator import MinecraftHTTPHandler
-from .ac_communicator import AirConditioner
+from minecraft_ac_controller.minecraft_communicator import MinecraftHTTPHandler
+from minecraft_ac_controller.ac_communicator import AirConditioner
 
 def start_ac_handler(ac, server_class=HTTPServer, handler_class=MinecraftHTTPHandler, port=8080):
     server_address = ('127.0.0.1', port)
@@ -14,7 +14,5 @@ def main():
     ac.connect_device()
     start_ac_handler(ac)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
