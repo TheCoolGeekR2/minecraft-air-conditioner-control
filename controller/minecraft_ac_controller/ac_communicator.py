e538bb462749b5543b75b366beadc69a43f5f43b
TheCoolGeek <TheCoolGeek@cool.com>
2025-04-26 01:01:17 +0300

    Created AC connection

---
+++ controller/minecraft_ac_controller/ac_communicator.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from greeclimate.discovery import Discovery, Listener
+from greeclimate.device import Device, DeviceInfo
+
+class AirConditioner:
+    def __init__(self):
+        pass
+
+    def connect_device(self):
+        discovery = Discovery()
+        for device_info in await discovery.scan(wait_for=5):
+            try:
+                device = Device(device_info)
+                await device.bind() # Device will auto bind on update if you omit this step
+            except CannotConnect:
+                _LOGGER.error("Unable to bind to gree device: %s", device_info)
+                continue
+        
+        self.device = device
+
