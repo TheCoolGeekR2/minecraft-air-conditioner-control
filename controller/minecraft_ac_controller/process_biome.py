c8cd5aa2fa03446492aef977935d5c67da9f6181
TheCoolGeek <4321@example.com>
2025-04-26 23:44:45 +0300

    integrated time of day into ac controller

--- controller/minecraft_ac_controller/process_biome.py
+++ controller/minecraft_ac_controller/process_biome.py
@@ -3,16 +3,10 @@ from .consts import BIOMES
 from .exceptions import BiomeNotFound
 from .ac_communicator import AirConditioner
 
-def process_biome(biome_data: Dict, air_conditioner: AirConditioner):
-    biome_id = biome_data["biome"]
-    biome_name = biome_id.replace("minecraft:", "")
-    
-    try:
-        ac_parameters = BIOMES[biome_name]
-    except KeyError:
-        raise BiomeNotFound(biome_name)
-    
+def process_biome(data: Dict, air_conditioner: AirConditioner):
+    biome_parameters = BIOMES[data['biome']]
+    temperature = biome_parameters["day_temp"] if data["time_of_day"] == 'DAY' else biome_parameters["night_temp"]
     air_conditioner.update_ac(
-        temp=ac_parameters["day_temp"],
-        fan_speed=ac_parameters["wind_level"] * 5
+        temp=temperature,
+        fan_speed=biome_parameters["wind_level"] * 5
     )
