315d303ec9215235871f7c9955ce33bfbfc0e3d6
TheCoolGeek <4321@example.com>
2025-04-26 23:47:08 +0300

    fixed key error

--- controller/minecraft_ac_controller/process_biome.py
+++ controller/minecraft_ac_controller/process_biome.py
@@ -4,7 +4,7 @@ from .exceptions import BiomeNotFound
 from .ac_communicator import AirConditioner
 
 def process_biome(data: Dict, air_conditioner: AirConditioner):
-    biome_parameters = BIOMES[data['biome']]
+    biome_parameters = BIOMES[data['biome'].replace("minecraft:", "")]
     temperature = biome_parameters["day_temp"] if data["time_of_day"] == 'DAY' else biome_parameters["night_temp"]
     air_conditioner.update_ac(
         temp=temperature,
